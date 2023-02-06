f6e6816084374ac41c0ed44be7d4b50a69714f1d
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-02-06 11:29:51 +0300

    feat: index js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,14 @@
-console.log('sadfdf')
+const form = document.querySelector("form");
+form.addEventListener('submit', async (event) => {
+  event.preventDefault()
+  if (checkValidity()) return;
+
+  const credentials = Object.fromEntries(new FormData(event.target));
+  try {
+    await signIn(credentials)
+  } catch {
+    await signUp(credentials);
+  }
+  form.classList.add('signup-form--success');
+  setTimeout(() => redirectToMain(), 500)
+})
